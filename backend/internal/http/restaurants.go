fbb204017a03425f86879fa193579096473fece6
Amadi Jerry <amadijerry310@gmail.com>
2024-10-31 13:29:12 +0100

    add get restaurants profile api

--- backend/internal/http/restaurants.go
+++ backend/internal/http/restaurants.go
@@ -8,6 +8,7 @@ import (
 	"github.com/JerryJeager/JeagerEats/internal/service/restaurants"
 	"github.com/JerryJeager/JeagerEats/internal/utils"
 	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
 )
 
 type RestaurantController struct {
@@ -93,4 +94,32 @@ func (c *RestaurantController) UpdateRestaurantIsActive(ctx *gin.Context) {
 	}
 
 	ctx.JSON(http.StatusOK, gin.H{"message": "Restaurant active status updated successfully"})
+}
+
+func (c *RestaurantController) GetRestaurantPublicProfile(ctx *gin.Context) {
+	var restaurantIDPathParam RestaurantIDPathParam
+	if err := ctx.ShouldBindUri(&restaurantIDPathParam); err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	id, err := uuid.Parse(restaurantIDPathParam.ID)
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	restaurant, err := c.serv.GetRestaurantPublicProfile(ctx, id)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	ctx.JSON(http.StatusOK, *restaurant)
+}
+
+func (c *RestaurantController) GetAllRestaurantPublicProfile(ctx *gin.Context) {
+	restaurants, err := c.serv.GetAllRestaurantPublicProfile(ctx)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	ctx.JSON(http.StatusOK, *restaurants)
 }
--- backend/internal/service/restaurants/service.go
+++ backend/internal/service/restaurants/service.go
@@ -11,6 +11,8 @@ type RestaurantSv interface {
 	UpdateRestaurant(ctx context.Context, userID uuid.UUID, restaurant *models.RestaurantUpdate) error
 	UpdateRestaurantProfileImg(ctx context.Context, userID uuid.UUID, filePath string) error
 	UpdateRestaurantIsActive(ctx context.Context, userID uuid.UUID, isActive *models.IsActive) error
+	GetRestaurantPublicProfile(ctx context.Context, id uuid.UUID) (*models.RestaurantPublicProfile, error)
+	GetAllRestaurantPublicProfile(ctx context.Context) (*models.RestaurantPublicProfileList, error)
 }
 
 type RestaurantServ struct {
@@ -32,3 +34,27 @@ func (s *RestaurantServ) UpdateRestaurantProfileImg(ctx context.Context, userID 
 func (s *RestaurantServ) UpdateRestaurantIsActive(ctx context.Context, userID uuid.UUID, isActive *models.IsActive) error {
 	return s.repo.UpdateRestaurantIsActive(ctx, userID, isActive.IsActive)
 }
+
+func (s *RestaurantServ) GetRestaurantPublicProfile(ctx context.Context, id uuid.UUID) (*models.RestaurantPublicProfile, error) {
+	r, err := s.repo.GetRestaurantPublicProfile(ctx, id)
+	if err != nil {
+		return nil, err
+	}
+	return &models.RestaurantPublicProfile{
+		ID:          r.ID,
+		Name:        r.Name,
+		Description: r.Description,
+		Address:     r.Address,
+		ProfileImg:  r.ProfileImg,
+		Rating:      r.Rating,
+		IsActive:    r.IsActive,
+		CreatedAt:   r.CreatedAt,
+		UpdatedAt:   r.UpdatedAt,
+		CuisineType: r.CuisineType,
+	}, nil
+}
+
+func (s *RestaurantServ) GetAllRestaurantPublicProfile(ctx context.Context) (*models.RestaurantPublicProfileList, error) {
+	return s.repo.GetAllRestaurantPublicProfile(ctx)
+}
+
