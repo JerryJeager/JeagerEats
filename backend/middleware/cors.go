be8c59b1a1b588f0f6fdb83e656ec419e17435f2
Amadi Jerry <amadijerry310@gmail.com>
2024-11-22 17:34:38 +0100

    fix cors issue

--- backend/middleware/cors.go
+++ backend/middleware/cors.go
@@ -1,20 +1,20 @@
 package middleware
 
 import (
-	"os"
+	// "os"
 
 	"github.com/gin-gonic/gin"
 )
 
 func CORSMiddleware() gin.HandlerFunc {
-	var origin string
-	if os.Getenv("ENVIRONMENT") == "development" {
-		origin = "http://localhost:3000"
-	} else {
-		origin = "https://jeagereats.vercel.app"
-	}
+	// var origin string
+	// if os.Getenv("ENVIRONMENT") == "development" {
+	// 	origin = "http://localhost:3000"
+	// } else {
+	// 	origin = "https://jeagereats.vercel.app"
+	// }
 	return func(c *gin.Context) {
-		c.Writer.Header().Set("Access-Control-Allow-Origin", origin)
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
 		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
 		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
 		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT, PATCH, DELETE")
