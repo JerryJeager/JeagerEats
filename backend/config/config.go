03694ee9922fb4b571902ab0ae836175b898e770
Amadi Jerry <amadijerry310@gmail.com>
2024-11-29 20:17:42 +0100

    minor db config changes

--- backend/config/config.go
+++ backend/config/config.go
@@ -52,7 +52,7 @@ func ConnectToDB() {
 	db.AutoMigrate(&models.Rider{})
 	db.AutoMigrate(&models.Menu{})
 
-	Session = db.Session(&gorm.Session{SkipDefaultTransaction: true})
+	Session = db.Session(&gorm.Session{SkipDefaultTransaction: true, PrepareStmt: true})
 	if Session != nil {
 		log.Print("success: created db session")
 	}
